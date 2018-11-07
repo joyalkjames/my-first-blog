c40d5edb3e7dcf0c24255d5b38f315daeaca36f3
Ajith K James <ajith.kjames3@gmail.com>
2018-11-07 21:03:15 +0530

    db

--- newsite/settings.py
+++ newsite/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'newsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'bloguser',
-        'PASSWORD':'password',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
